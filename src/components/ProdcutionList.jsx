68e156a7b02c8c8dc747e1f11bcb22fabf6cd179
johnnywalker.appkey@gmail.com <johnnywalker.appkey@gmail.com>
2023-06-06 13:33:08 +0800

    hapus video

--- src/components/ProdcutionList.jsx
+++ src/components/ProdcutionList.jsx
@@ -1,47 +1,36 @@
 /* eslint-disable no-unused-vars */
 import React from "react";
 import disney from "./../assets/images/disney.png";
-import marvel from "./../assets/Images/marvel.png";
-import nationalG from "./../assets/Images/nationalG.png";
-import pixar from "./../assets/Images/pixar.png";
-import starwar from "./../assets/Images/starwar.png";
-import starwarV from "./../assets/Videos/star-wars.mp4";
-import disneyV from "./../assets/Videos/disney.mp4";
-import marvelV from "./../assets/Videos/marvel.mp4";
-import nationalGeographicV from "./../assets/Videos/national-geographic.mp4";
-import pixarV from "./../assets/Videos/pixar.mp4";
+import marvel from "./../assets/images/marvel.png";
+import nationalG from "./../assets/images/nationalG.png";
+import pixar from "./../assets/images/pixar.png";
+import starwar from "./../assets/images/starwar.png";
 
 function ProdcutionList() {
   const productionHouseList = [
     {
       id: 1,
       image: pixar,
-      video: pixarV,
     },
     {
       id: 2,
       image: marvel,
-      video: marvelV,
     },
     {
       id: 3,
       image: starwar,
-      video: starwarV,
     },
     {
       id: 4,
       image: nationalG,
-      video: nationalGeographicV,
     },
     {
       id: 5,
       image: starwar,
-      video: starwarV,
     },
     {
       id: 6,
       image: marvel,
-      video: marvelV,
     },
   ];
   return (
